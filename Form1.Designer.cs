7e18956e9f7115bd669ae7dd78eb411236bb39b4
agreat10 <komolov10@gmail.com>
2023-06-09 16:55:26 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace WindowsFormsDBAccess
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShow = new System.Windows.Forms.Button();
+            this.lblShow = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnShow
+            // 
+            this.btnShow.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnShow.Location = new System.Drawing.Point(308, 58);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(111, 49);
+            this.btnShow.TabIndex = 0;
+            this.btnShow.Text = "Показать";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // lblShow
+            // 
+            this.lblShow.AutoSize = true;
+            this.lblShow.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblShow.Location = new System.Drawing.Point(66, 70);
+            this.lblShow.Name = "lblShow";
+            this.lblShow.Size = new System.Drawing.Size(64, 25);
+            this.lblShow.TabIndex = 1;
+            this.lblShow.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblShow);
+            this.Controls.Add(this.btnShow);
+            this.Name = "Form1";
+            this.Text = "Работа с базой данных ACCESS";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Label lblShow;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsDBAccess
+{
+    
+    public partial class Form1 : Form
+    {
+        public static string connectString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\\myFolder\\Database1.mdb;Persist Security Info=False;";
+
+        private OleDbConnection myConnection = null;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            myConnection = new OleDbConnection(connectString);
+            myConnection.Open();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            myConnection.Close();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT FirstName FROM [Students] WHERE Id = 1";
+            OleDbCommand oleDbCommand = new OleDbCommand(query,myConnection);
+            lblShow.Text = oleDbCommand.ExecuteScalar().ToString();
+        }
+    }
+}
